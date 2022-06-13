775a85c3fd044554598ae53cd261b4633c88afcc
MasahiroAraki <rm0178vk@ed.ritsumei.ac.jp>
2022-06-13 19:57:40 +0900

    5syou

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -1,26 +1,26 @@
 # coding: utf-8
 
 
-class MulLayer:
-    def __init__(self):
+class MulLayer: #乗算レイヤ
+    def __init__(self): #インスタンス変数であるxとyの初期化を行う(順伝播時の入力値を保持するため)
         self.x = None
         self.y = None
 
-    def forward(self, x, y):
+    def forward(self, x, y): #2つの引数を受け取る
         self.x = x
         self.y = y                
         out = x * y
 
         return out
 
-    def backward(self, dout):
+    def backward(self, dout): #上流から流れてきた微分(dout)に対し，順伝播の”ひっくり返した値”を乗算して下流に流す
         dx = dout * self.y
         dy = dout * self.x
 
         return dx, dy
 
 
-class AddLayer:
+class AddLayer: #加算レイヤ
     def __init__(self):
         pass
 
